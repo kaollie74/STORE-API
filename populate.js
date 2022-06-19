f05aafd9eadcdcc888c83e263db87210f9cc6ba2
Kyle Olson <kaollie74@gmail.com>
2022-06-19 16:15:42 -0500

    more formatting

--- populate.js
+++ populate.js
@@ -1,23 +1,21 @@
-require('dotenv').config()
+require("dotenv").config();
 
-
-const connectDB = require('./db/connect')
-const ProductModel = require('./models/product.model')
-const jsonProducts = require('./products.json')
+const connectDB = require("./db/connect");
+const ProductModel = require("./models/product.model");
+const jsonProducts = require("./products.json");
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI)
-        await ProductModel.deleteMany()
-        await ProductModel.create(jsonProducts)
+        await connectDB(process.env.MONGO_URI);
+        await ProductModel.deleteMany();
+        await ProductModel.create(jsonProducts);
 
-        console.log('success!')
+        console.log("success!");
         process.exit(0);
     } catch (e) {
-        console.log('ERROR >>>>', e);
+        console.log("ERROR >>>>", e);
         process.exit(1);
-
     }
-}
+};
 
 start();
